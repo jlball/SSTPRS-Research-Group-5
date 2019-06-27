65496a10bde60003603502f5d5be2089c4adcdb0
Alexander Brown <alexander_brown3@brown.edu>
2019-06-27 15:12:09 -0400

    Update GeneratingIncidenceMatrices.py

--- GeneratingIncidenceMatrices.py
+++ GeneratingIncidenceMatrices.py
@@ -11,18 +11,30 @@ def gLMat(line1, line2, line3, line4):
     return matrix
 
 
+# def liftBosons (inputMatrix, bosons):
+#     VIMatrix = inputMatrix
+#     for z in range(0, 4):
+#         #print("Boson #: " + str(bosons[z]))
+#         if bosons[z] == 1:
+#             #print("Lifted")
+#             for x in range(0, 16):
+#                 if inputMatrix[z, x] == 1:
+#                     for y in range(0, 8):
+#                         VIMatrix[y, x] = - inputMatrix[y, x]
+#     return VIMatrix;
+
 def liftBosons (inputMatrix, bosons):
-    VIMatrix = inputMatrix
+    # VIMatrix = inputMatrix
     for z in range(0, 4):
         #print("Boson #: " + str(bosons[z]))
         if bosons[z] == 1:
             #print("Lifted")
             for x in range(0, 16):
-                if inputMatrix[z, x] == 1:
+                if z == x%4:
                     for y in range(0, 8):
-                        VIMatrix[y, x] = - inputMatrix[y, x]
-    return VIMatrix;
-        
+                        inputMatrix[y, x] = - inputMatrix[y, x]
+    return inputMatrix;
+
 
 #initialization of the AllL and listOfL matrices for use in the populating of AllL
 listOfL = []
@@ -46,7 +58,7 @@ bigIdentity = np.concatenate((identity, identity, identity, identity), 1)
 #Initializing the list which will contain all incidence matrices for valise adinkras
 AllValiseIncidence = []
 
-#Generating all incidence matrices by concatenating the bigIdentity and the transpose abs value of the L matrices 
+#Generating all incidence matrices by concatenating the bigIdentity and the transpose abs value of the L matrices
 for x in range(0, 96):
     LmatrixPart = np.concatenate((AllL[x][0], AllL[x][1], AllL[x][2], AllL[x][3]), 1, )
     AllValiseIncidence.append(np.concatenate((bigIdentity, LmatrixPart), 0))
@@ -73,10 +85,26 @@ allBosonConfigs = np.array([[0, 0, 0, 0],
 AllVI = AllValiseIncidence
 allV = []
 
+# pp.pprint(AllValiseIncidence)
+print("")
+print("")
+print("")
 pp.pprint(AllValiseIncidence[0])
-allV.append(liftBosons(AllValiseIncidence[0], allBosonConfigs[15]))
+# for i in range (0, 16):
+#     allV = []
+#     allV.append(liftBosons(AllValiseIncidence[0], allBosonConfigs[i]))
+#     pp.pprint(allV)
+for i in range (0, 16):
+    allV = []
+    allV.append(liftBosons(AllVI[0], allBosonConfigs[i]))
+    pp.pprint(allV)
+    print("")
+# allV.append(liftBosons(AllValiseIncidence[0], allBosonConfigs[6]))
+#allV.append(liftBosons(AllValiseIncidence[0], allBosonConfigs[2]))
 pp.pprint(AllValiseIncidence[0])
 pp.pprint(allV)
+print("")
+print("")
 
-   
+
 #Hmm very interesting
